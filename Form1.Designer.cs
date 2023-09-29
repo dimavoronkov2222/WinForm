5e119815c21700ddeafb96721c50c85b2ffe2dae
Дмитро Воронков <dimavoronkov2222@hotmail.com>
2023-09-29 13:35:40 +0200

    add code lines and edit code lines

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,8 +32,18 @@
             label2 = new Label();
             label3 = new Label();
             Surname = new TextBox();
-            Name = new TextBox();
+            Name1 = new TextBox();
             Patronymic = new TextBox();
+            label4 = new Label();
+            Gender_of_a_person = new TextBox();
+            label5 = new Label();
+            Year_and_date_of_birth = new TextBox();
+            label6 = new Label();
+            Family_status = new TextBox();
+            label7 = new Label();
+            Additional_Information = new TextBox();
+            save_button = new Button();
+            saveFileDialog1 = new SaveFileDialog();
             SuspendLayout();
             // 
             // label1
@@ -70,12 +80,12 @@
             Surname.Size = new Size(102, 27);
             Surname.TabIndex = 3;
             // 
-            // Name
+            // Name1
             // 
-            Name.Location = new Point(195, 78);
-            Name.Name = "Name";
-            Name.Size = new Size(102, 27);
-            Name.TabIndex = 4;
+            Name1.Location = new Point(195, 78);
+            Name1.Name = "Name1";
+            Name1.Size = new Size(102, 27);
+            Name1.TabIndex = 4;
             // 
             // Patronymic
             // 
@@ -84,19 +94,101 @@
             Patronymic.Size = new Size(102, 27);
             Patronymic.TabIndex = 5;
             // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(54, 118);
+            label4.Name = "label4";
+            label4.Size = new Size(136, 20);
+            label4.TabIndex = 6;
+            label4.Text = "Gender of a person";
+            // 
+            // Gender_of_a_person
+            // 
+            Gender_of_a_person.Location = new Point(54, 141);
+            Gender_of_a_person.Name = "Gender_of_a_person";
+            Gender_of_a_person.Size = new Size(125, 27);
+            Gender_of_a_person.TabIndex = 7;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(226, 118);
+            label5.Name = "label5";
+            label5.Size = new Size(153, 20);
+            label5.TabIndex = 8;
+            label5.Text = "Year and date of birth";
+            // 
+            // Year_and_date_of_birth
+            // 
+            Year_and_date_of_birth.Location = new Point(226, 141);
+            Year_and_date_of_birth.Name = "Year_and_date_of_birth";
+            Year_and_date_of_birth.Size = new Size(153, 27);
+            Year_and_date_of_birth.TabIndex = 9;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(472, 65);
+            label6.Name = "label6";
+            label6.Size = new Size(93, 20);
+            label6.TabIndex = 10;
+            label6.Text = "Family status";
+            // 
+            // Family_status
+            // 
+            Family_status.Location = new Point(457, 88);
+            Family_status.Name = "Family_status";
+            Family_status.Size = new Size(125, 27);
+            Family_status.TabIndex = 11;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(440, 118);
+            label7.Name = "label7";
+            label7.Size = new Size(161, 20);
+            label7.TabIndex = 12;
+            label7.Text = "Additional Information";
+            // 
+            // Additional_Information
+            // 
+            Additional_Information.Location = new Point(440, 146);
+            Additional_Information.Name = "Additional_Information";
+            Additional_Information.Size = new Size(161, 27);
+            Additional_Information.TabIndex = 13;
+            // 
+            // save_button
+            // 
+            save_button.Location = new Point(265, 211);
+            save_button.Name = "save_button";
+            save_button.Size = new Size(94, 44);
+            save_button.TabIndex = 14;
+            save_button.Text = "Save";
+            save_button.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(702, 433);
+            Controls.Add(save_button);
+            Controls.Add(Additional_Information);
+            Controls.Add(label7);
+            Controls.Add(Family_status);
+            Controls.Add(label6);
+            Controls.Add(Year_and_date_of_birth);
+            Controls.Add(label5);
+            Controls.Add(Gender_of_a_person);
+            Controls.Add(label4);
             Controls.Add(Patronymic);
-            Controls.Add(Name);
+            Controls.Add(Name1);
             Controls.Add(Surname);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Get information person";
             ResumeLayout(false);
             PerformLayout();
         }
@@ -107,7 +199,17 @@
         private Label label2;
         private Label label3;
         private TextBox Surname;
-        private TextBox Name;
+        private TextBox Name1;
         private TextBox Patronymic;
+        private Label label4;
+        private TextBox Gender_of_a_person;
+        private Label label5;
+        private TextBox Year_and_date_of_birth;
+        private Label label6;
+        private TextBox Family_status;
+        private Label label7;
+        private TextBox Additional_Information;
+        private Button save_button;
+        private SaveFileDialog saveFileDialog1;
     }
 }
