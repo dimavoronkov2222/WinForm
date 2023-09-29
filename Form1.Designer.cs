0967e2df581325b37e652dc836700f910d6abe2c
Дмитро Воронков <dimavoronkov2222@hotmail.com>
2023-09-29 18:44:55 +0200

    add lines

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            piker1 = new DateTimePicker();
+            piker2 = new DateTimePicker();
+            label1 = new Label();
+            result = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // piker1
+            // 
+            piker1.Location = new Point(75, 92);
+            piker1.Name = "piker1";
+            piker1.Size = new Size(250, 27);
+            piker1.TabIndex = 0;
+            // 
+            // piker2
+            // 
+            piker2.Location = new Point(376, 92);
+            piker2.Name = "piker2";
+            piker2.Size = new Size(250, 27);
+            piker2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(329, 181);
+            label1.Name = "label1";
+            label1.Size = new Size(49, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Result";
+            // 
+            // result
+            // 
+            result.AutoSize = true;
+            result.Location = new Point(344, 215);
+            result.Name = "result";
+            result.Size = new Size(17, 20);
+            result.TabIndex = 3;
+            result.Text = "0";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(326, 149);
+            button1.Name = "button1";
+            button1.Size = new Size(59, 29);
+            button1.TabIndex = 4;
+            button1.Text = "count";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(result);
+            Controls.Add(label1);
+            Controls.Add(piker2);
+            Controls.Add(piker1);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private DateTimePicker piker1;
+        private DateTimePicker piker2;
+        private Label label1;
+        private Label result;
+        private Button button1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,12 @@ namespace WinForm
         {
             InitializeComponent();
         }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime date1 = piker1.Value;
+            DateTime date2 = piker2.Value;
+            int days = (date2 - date1).Days;
+            result.Text = Convert.ToString(days) + " Days";
+        }
     }
 }
